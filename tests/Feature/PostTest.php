0afcd219f41793b2ccadda031887014aa17cb331
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-07-31 14:19:24 -0300

    feat: Criando arquivo de validação e  realizando testes

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_post_creation_with_no_titulo()
+    {
+        /** 
+         * Testing post registry with no titulo
+        */
+        $response = $this->post(route('post.store'), [
+            "titulo" => 7484,
+            "user_id" => "4725",
+            "body" => "ashdafsdghkjl;"
+        ]);
+        
+        $response->assertStatus(302);
+    }
+
+    public function test_post_creation_with_no_body()
+    {
+        /** 
+         * Testing post registry with no body
+        */
+        $response = $this->post(route('post.store'), [
+            "titulo" => "Teste",
+            "user_id" => "4725",
+            "body" => 185151
+        ]);
+        
+        $response->assertStatus(302);
+
+    }
+}
